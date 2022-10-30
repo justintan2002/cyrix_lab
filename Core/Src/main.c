939e6ba20984b8cb6efc9c86c524ff6ecbf6cbc0
teohxuen <projectxuen@gmail.com>
2022-10-30 20:30:33 +0800

    changed passing in pointer of mode to just passing value of mode into violations count function

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -32,7 +32,7 @@ void SystemClock_Config(void);
 void change_mode(void);
 void led_handler(uint8_t mode, uint8_t warning);
 void uart_print(const char* format, ...);
-uint8_t violations_count(uint8_t arr[], uint8_t *warning, uint8_t *mode);
+uint8_t violations_count(uint8_t arr[], uint8_t *warning, uint8_t mode);
 
 uint32_t t1 = 0, t2 = 0;
 uint32_t led_tick  = 0;
@@ -166,7 +166,7 @@ int main(void)
 			if (accel_data[2] < accel_th[0] || accel_data[2] > accel_th[1]){
 				violations[0] = 1;
 				uart_print("A:%.2f m/s2 violated threshold\n", accel_data[2]);
-				v_count = violations_count(violations, &warning, &mode); //count number of violations and update mode accordingly
+				v_count = violations_count(violations, &warning, mode); //count number of violations and update mode accordingly
 				uart_print("%d sensors violated threshold\n", v_count);
 			}
 
@@ -174,7 +174,7 @@ int main(void)
 			if (gyro_data[2] > gyro_th) {
 				violations[1] = 1;
 				uart_print("G:%.2f dps violated threshold\n", gyro_data[2]);
-				v_count = violations_count(violations, &warning, &mode); //count number of violations and update mode accordingly
+				v_count = violations_count(violations, &warning, mode); //count number of violations and update mode accordingly
 				uart_print("%d sensors violated threshold\n", v_count);
 			}
 
@@ -182,7 +182,7 @@ int main(void)
 			if (mag_magnitude > mag_th){
 				violations[2] = 1;
 				uart_print("M:%.3f gauss violated threshold\n", mag_magnitude);
-				v_count = violations_count(violations, &warning, &mode); //count number of violations and update mode accordingly
+				v_count = violations_count(violations, &warning, mode); //count number of violations and update mode accordingly
 				uart_print("%d sensors violated threshold\n", v_count);
 			}
 
@@ -198,7 +198,7 @@ int main(void)
 			if (temp_data > temp_th){
 				violations[3] = 1;
 				uart_print("T:%.2f C violated threshold\n", temp_data);
-				v_count = violations_count(violations, &warning, &mode); //count number of violations and update mode accordingly
+				v_count = violations_count(violations, &warning, mode); //count number of violations and update mode accordingly
 				uart_print("%d sensors violated threshold\n", v_count);
 			}
 
@@ -206,7 +206,7 @@ int main(void)
 			if (humidity_data < humid_th){
 				violations[4] = 1;
 				uart_print("H:%.2f %%rH violated threshold\n", humidity_data);
-				v_count = violations_count(violations, &warning, &mode); //count number of violations and update mode accordingly
+				v_count = violations_count(violations, &warning, mode); //count number of violations and update mode accordingly
 				uart_print("%d sensors violated threshold\n", v_count);
 			}
 		}
@@ -220,7 +220,7 @@ int main(void)
 			if (pressure_data < pressure_th[0] || pressure_data > pressure_th[1]){
 				violations[5] = 1;
 				uart_print("P:%.2f hPa violated threshold. Number of Violations: %d\n", pressure_data, violations);
-				v_count = violations_count(violations, &warning, &mode); //count number of violations and update mode accordingly
+				v_count = violations_count(violations, &warning, mode); //count number of violations and update mode accordingly
 				uart_print("%d sensors violated threshold\n", v_count);
 			}
 		}
@@ -295,12 +295,12 @@ int check_button(){
 	}
 }
 
-uint8_t violations_count(uint8_t arr[], uint8_t *warning, uint8_t *mode){
+uint8_t violations_count(uint8_t arr[], uint8_t *warning, uint8_t mode){
 	uint8_t v_count = 0;
 	for (int i=0; i<6; i++){
 		if (arr[i] == 1) v_count += 1;
 	}
-	if ((*mode == 0 && v_count >= 2) || (*mode==1 && v_count >= 1)){
+	if ((mode == 0 && v_count >= 2) || (mode==1 && v_count >= 1)){
 		*warning = 1;
 	}
 	return v_count;
