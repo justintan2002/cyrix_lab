984aa4ff82b6ff2e808aa0c5744895bb58881800
teohxuen <projectxuen@gmail.com>
2022-10-30 17:12:06 +0800

    remvoed gitignore files

--- Debug/Drivers/BSP/Components/M24SR/m24sr_v1.1.d
+++
@@ -1,4 +0,0 @@
-Drivers/BSP/Components/M24SR/m24sr_v1.1.o: \
- ../Drivers/BSP/Components/M24SR/m24sr_v1.1.c \
- ../Drivers/BSP/Components/M24SR/m24sr_v1.1.h
-../Drivers/BSP/Components/M24SR/m24sr_v1.1.h:
